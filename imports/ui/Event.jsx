e79669352681d0d78f180e7737a769877f1c8299
HuggingFox <joshua.kern99@hotmail.com>
2019-08-15 11:24:32 +0200

    Start

---
+++ imports/ui/Event.jsx
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { Meteor } from 'meteor/meteor';
+import classnames from 'classnames';
+
+// Task component - represents a single todo item
+export default class Task extends Component {
